50328c7dadda476d341678f9c993d84413a1a24d
Jun0422 <juv317life@yahoo.co.jp>
2021-06-27 00:18:52 +0900

    解答_挑戦問題

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,13 @@
 object Main {
 
+  implicit class RichString(val src: String) {
+    def twice: String = src + src
+    def increase(count: Int): String = {
+      var result: String = ""
+      for(_ <- 0 to  count) {
+        result += src
+      }
+      result
+    }
+  }
 }
